9598831146639e48b49636d2a5fd690ce1b97755
James Hughes <hughes.james@gmail.com>
2015-03-28 11:06:16 -0700

    POST to /themes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Workshop' });
 });
 
+router.post('/themes', function(req, res, next) {
+  var theme = req.body.theme;
+  res.render('themes', {});
+  console.log(theme);
+});
 module.exports = router;
